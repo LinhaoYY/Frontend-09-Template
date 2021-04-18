5af24230e66ff06f79f277eee8ade0c63f1ca79e
LinhaoYY <2416752229@qq.com>
2021-04-18 22:27:20 +0800

    提交

---
+++ Week_08/server.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const { request } = require("node:http");
+http.createServer((request, require) => {
+    let body = [];
+    request.on('error', (err) => {
+        console.log(err)
+    }).on('data', (chunk) => {
+        body.push(chunk.toString())
+    }).on("end", () => {
+        body = Buffer.concat(body).toString()
+        console.log('body', body);
+        request.writeHead(200, {'Content-Type': 'Text/html'})
+        request.end('Hello world\n')
+    }).listen(8088)
+})
+console.log('serve started');
